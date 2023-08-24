d3ef41665f45702deec8d1e65fe76460d088eccf
Darya <kubasovada@mail.ru>
2023-08-24 11:21:11 +0300

    del testdata

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 
 import java.time.Duration;
 import java.util.List;
+//import src.main.test.java.DataTest;
+
 
 
 public class MainPage {
@@ -44,30 +46,19 @@ public class MainPage {
         }
     }
 
-    String[] Answers = {"Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
-            "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.",
-            "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.",
-            "Только начиная с завтрашнего дня. Но скоро станем расторопнее.",
-            "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.",
-            "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.",
-            "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.",
-            "Да, обязательно. Всем самокатов! И Москве, и Московской области."};
-
-
     public void scrollTo(WebElement elementToScroll) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", elementToScroll);
     }
 
     public boolean isEveryFaqAnswerCorrect() {
+        //DataTest dataTest = new DataTest;
         for (int i = 0; i < getQuestions().size(); i++) {
             scrollTo(getQuestions().get(i));
             getQuestions().get(i).isDisplayed();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
             getQuestions().get(i).click();
             getAnswers().get(i).isDisplayed();
-            String answerItemFromWebElements = getAnswers().get(i).getText();
-            String answerFromArray = Answers[i];
-            if (!(answerItemFromWebElements.equals(answerFromArray))) {
+            if (!(getAnswers().get(i).isDisplayed())) {
                 return false;
             }
 
